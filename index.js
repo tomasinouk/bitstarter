782ffcad0bdb852116d403eb915986fcd1c34f24
Tomas Blaha <tomasblaha@gmail.com>
2014-05-03 05:17:23 +0000

    Update index.js

--- index.js
+++ index.js
@@ -2,14 +2,16 @@ var fs = require('fs');
 
 var express = require('express')
 var app = express();
-fs.readFileSync('index.html');
-var file_content = new Buffer(fs.readFileSync('index.html'));
 
 
-App.set('port', (process.env.PORT || 5000))
+
+app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 // read file 'index.html'
+fs.readFileSync('index.html');
+var file_content = new Buffer(fs.readFileSync('index.html'));
+
 
 app.get('/', function(request, response) {
     response.send(file_content.toString('utf-8'))
@@ -18,4 +20,6 @@ app.get('/', function(request, response) {
 
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
+    console.log(file_content.toString('utf-8'))
+
 })
