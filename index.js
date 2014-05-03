c76abf3e61cb663f22c4d3dcff0781eeb46534fb
Tomas Blaha <tomasblaha@gmail.com>
2014-05-03 04:56:27 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
+var fs = require('fs');
+
 var express = require('express')
 var app = express();
 
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
+// read file 'index.html'
+
+fs.readFileSync('index.html');
+var file_content = new Buffer(fs.readFileSync('index.html');
+file_content.toString
 app.get('/', function(request, response) {
-  response.send('Hello World 2!')
+    response.send(file_content.toString('utf-8'))
+//  response.send('Hello World 2!')
 })
 
 app.listen(app.get('port'), function() {
